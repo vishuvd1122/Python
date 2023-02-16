3085b38ef56fcd95a2b2b892519e96bb7dd55389
vishuvd1122 <122728138+vishuvd1122@users.noreply.github.com>
2023-02-16 21:59:48 +0530

    Update Area of triangle.py

--- Area of triangle.py
+++ Area of triangle.py
@@ -1,5 +1,4 @@
-# Program to calculate the area of triangle by its formula
 b = float(input("Enter base of a triangle:"))
 h = float(input("Enter height of a triangle:"))
-area = 0.5*b*h                                    # "area" will store the calculated value of area of triangle
+area = 0.5*b*h                                  
 print("Area of triangle with base and heigth of", b, "units and", h, "units respectively:", area, "sq. units")
