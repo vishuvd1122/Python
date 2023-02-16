89b0b277d70a9362294a320de323c09953fe507e
vishuvd1122 <122728138+vishuvd1122@users.noreply.github.com>
2023-02-16 22:09:36 +0530

    Update Area of rhombus.py

--- Area of rhombus.py
+++ Area of rhombus.py
@@ -1,5 +1,4 @@
-# Program to calculate the area of rhombus when both the diagonals are taken as input by the user
 d1 = float(input("Enter first diagonal of a rhombus:"))
 d2 = float(input("Enter second diagonal of a rhombus:"))
-area = 0.5*d1*d2                                               # Formula to calculate area of rhombus
+area = 0.5*d1*d2                                              
 print("Area of rhombus with diagonals of", d1, "units and", d2, "units respectively:", area, "sq. units")
